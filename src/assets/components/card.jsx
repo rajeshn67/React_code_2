a89b70e72f805d72dcfe02d8552749775253e125
rajeshn67 <rajeshnarwade67@gmail.com>
2025-02-26 14:57:24 +0530

    code is added

---
+++ src/assets/components/card.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Button = (props) => {
+  return (
+    <div>
+       {props.children}
+       <button onClick={props.handleClick}>{props.text}</button>
+      
+    </div>
+  )
+}
+
+export default Button
